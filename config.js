326f928100acf44547c963bd35938ad6d7fa4ac7
huy-bao-ne <144243646+huy-bao-ne@users.noreply.github.com>
2023-12-17 18:29:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = ' dấu iu';
+const giftUrl = 'https://www.facebook.com/huybao.nguyen.7927408';
+const eventName = 'To AE1';
+const titleCard = 'Gift exchange invitation';
+const contentCard = 'Hãy nhớ tham gia gift exchange start at 9AM on 22/12/2023 at room 112 nhé (From huybao with luv)';
+const giftImage = '';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
